e5f6dc3f8b8a26cfe89a4ab964e76a7eda331a70
Aaron Su <aaronsu11@outlook.com>
2022-08-30 23:42:13 +1000

    add build files

--- robot_ws/src/hello_world_robot/nodes/rotate.py
+++ robot_ws/src/hello_world_robot/nodes/rotate.py
@@ -31,7 +31,8 @@ class Rotator():
 
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
-            self.twist.angular.z = 0.1
+            self.twist.linear.x = 0.2
+            # self.twist.angular.z = 0.1
             self._cmd_pub.publish(self.twist)
             rospy.loginfo('Rotating robot: %s', self.twist)
             r.sleep()
